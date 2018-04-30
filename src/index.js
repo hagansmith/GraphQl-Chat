defd852ec29f3f5b8aa26d76d2016b0ba6d6508c
Adam Smith <hagansmith@gmail.com>
2018-04-30 07:53:05 -0500

    chat app

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+const wsLink = new WebSocketLink({
+
+    uri: 'wss://subscriptions.us-west-2.graph.cool/v1/cjgl2c2d60y1801622vo4ux9x',
+    options: {
+      reconnect: true
+    }
+})
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjgl2c2d60y1801622vo4ux9x' })
+
+const link = split(
+
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    httpLink,
+)
+
+const client = new ApolloClient({
+
+    link,
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
